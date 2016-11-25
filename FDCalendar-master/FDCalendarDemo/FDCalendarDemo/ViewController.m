53f31983c7c31aeb611791d95e7489a930330a87
liujingcheng <850238466@qq.com>
2016-11-25 14:31:23 +0800

    no message

---
+++ FDCalendar-master/FDCalendarDemo/FDCalendarDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  FDCalendarDemo
+//
+//  Created by fergusding on 15/8/20.
+//  Copyright (c) 2015年 fergusding. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "FDCalendar.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    
+    FDCalendar *calendar = [[FDCalendar alloc] initWithCurrentDate:[NSDate date]];
+    CGRect frame = calendar.frame;
+    frame.origin.y = 20;
+    calendar.frame = frame;
+    [self.view addSubview:calendar];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
